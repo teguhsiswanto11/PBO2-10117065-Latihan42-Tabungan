6b21bd97311e1613a1a136a345366b117d055553
Teguh Siswanto <tsiswanto73@gmail.com>
2018-10-20 05:06:25 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * @author
+ * NAMA     : Teguh Siswanto
+ * KELAS    : PBO2
+ * NIM      : 10117065
+ * Deskripsi Program : Mengambil uang dari saldo yang diinputkan oleh user dan menampilkan
+ * sisa saldo setelah melakukan penarikan
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn1 = new Scanner(System.in);
+        Tabungan tab = new Tabungan();
+
+        System.out.println("===Program Penarikan Uang===");
+        System.out.print("Masukkan Saldo Awal : ");
+        int x = tab.tabungan(scn1.nextInt());
+        System.out.print("Jumlah Uang yang Diambil : ");
+        int y = tab.ambilUang(scn1.nextInt());
+        System.out.println("Saldo Anda Sekarang : "+(tab.tabungan(x)-tab.ambilUang(y)));
+
+    }
+}
---
+++ src/Tabungan.java
@@ -0,0 +1,12 @@
+public class Tabungan {
+    private int saldo;
+
+    public int tabungan(int saldo) {
+
+        return this.saldo = saldo;}
+
+    public int ambilUang(int jumlah) {
+
+        return jumlah;}
+
+}
